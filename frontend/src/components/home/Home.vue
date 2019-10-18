065179647bdf1aa9d1976ea67ddb9816fe18d748
Felipe Ribeiro <felipeboy_157@hotmail.com>
2019-10-18 10:44:53 -0300

    Estatisticas

--- frontend/src/components/home/Home.vue
+++ frontend/src/components/home/Home.vue
@@ -2,6 +2,14 @@
     <div class="home">
         <PageTitle icon="fa fa-home" main="Dashboard"
                    sub="Base de Conhecimento" />
+        <div class="stats">
+            <Stat title="Categorias" :value="stat.categories"
+                  icon="fa fa-folder" color="#d54d50" />
+            <Stat title="Artigos" :value="stat.articles"
+                  icon="fa fa-file" color="#3bc480" />
+            <Stat title="Usuários" :value="stat.users"
+                  icon="fa fa-user" color="#3282cd" />
+        </div>
     </div>
 </template>
 
@@ -22,11 +30,19 @@
         methods: {
             getStats() {
                 axios.get(`${baseApiUrl}/stats`)
+                    .then(res => this.stat = res.data)
             }
+        },
+        mounted() {
+            this.getStats()
         }
     }
 </script>
 
 <style>
-
+    .stats {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,6 +9,9 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+// TEMPORÁRIO!
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTAsIm5hbWUiOiJGZWxpcGUiLCJlbWFpbCI6ImZlbGlwZUBlbWFpbC5jb20iLCJhZG1pbiI6InRydWUiLCJpYXQiOjE1NzE0MDI5MDcsImV4cCI6MTU3MTY2MjEwN30.go3iwZXHR2rslJWUyYuXyLZr2gp2B20HQEzb2bHuRiY'
+
 new Vue({
   store,
   router,
