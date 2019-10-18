645c8edc2daab507a4cc93bb38d3cd1e414d1a9e
felink21 <felipeboy_157@hotmail.com>
2019-10-17 21:35:59 -0300

    template menu

---
+++ frontend/src/components/template/Content.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="content"></div>
+</template>
+
+<script>
+    export default {
+        name: "Content"
+    }
+</script>
+
+<style scoped>
+    .content {
+        grid-area: content;
+        background-color: #efefef;
+        paddig: 20px
+    }
+</style>
