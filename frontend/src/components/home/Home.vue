bd22f9977f0218ca1351d5e0876e7e71b413d5f2
felink21 <felipeboy_157@hotmail.com>
2019-10-18 00:34:30 -0300

    preparando consumo api

--- frontend/src/components/home/Home.vue
+++ frontend/src/components/home/Home.vue
@@ -6,11 +6,24 @@
 </template>
 
 <script>
-    import PageTitle from "../template/PageTitle"
+    import PageTitle from '../template/PageTitle'
+    import Stat from './Stat'
+    import axios from 'axios'
+    import { baseApiUrl} from '../../global';
 
     export default {
         name: "Home",
-        components: { PageTitle }
+        components: { PageTitle, Stat },
+        data: function () {
+            return {
+                stat: {}
+            }
+        },
+        methods: {
+            getStats() {
+                axios.get(`${baseApiUrl}/stats`)
+            }
+        }
     }
 </script>
 
