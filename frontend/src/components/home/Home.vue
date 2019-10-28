f565957c8da41ccb679de939d385dfeaaa124c89
Felipe Ribeiro <felipeboy_157@hotmail.com>
2019-10-28 02:07:38 -0300

    faltando mover content qd menu sair

--- frontend/src/components/home/Home.vue
+++ frontend/src/components/home/Home.vue
@@ -17,20 +17,19 @@
     import PageTitle from '../template/PageTitle'
     import Stat from './Stat'
     import axios from 'axios'
-    import { baseApiUrl} from '../../global';
+    import { baseApiUrl } from '@/global'
 
     export default {
-        name: "Home",
+        name: 'Home',
         components: { PageTitle, Stat },
-        data: function () {
+        data: function() {
             return {
                 stat: {}
             }
         },
         methods: {
             getStats() {
-                axios.get(`${baseApiUrl}/stats`)
-                    .then(res => this.stat = res.data)
+                axios.get(`${baseApiUrl}/stats`).then(res => this.stat = res.data)
             }
         },
         mounted() {
